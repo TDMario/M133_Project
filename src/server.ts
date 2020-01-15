2f8d3212043e895e69d5757d8d9716ae9766451f
TDMario <mario_forrer@sluz.ch>
2020-01-15 14:47:09 +0100

    + Create Base + Update gitignore

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+
+import * as express from 'express';
+import * as expressLayouts from 'express-ejs-layouts';
+import * as expressSession from 'express-session';
+
+const port = 8080;
+
+
+const app = express();
+app.use(expressSession({
+    secret: "save",
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Zuständig für richtige darstellung von Daten
+app.use(express.urlencoded({ extended: true }));
+
+
+// EJS für views
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
+
+/* Viewverlinkungen */
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__direname, "/View/home.html"))
+// });
+
+// app.get("/detail", (req, res) => {
+//     res.sendFile(path.join(__direname, "/View/detail.html"))
+// });
+
